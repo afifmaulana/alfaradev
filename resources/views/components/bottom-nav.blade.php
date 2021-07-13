aa5c1c9114889419eb5180792411741ff93631b8
Afif Maulana Iskandar <afifmaulanaiskandar@gmail.com>
2021-07-13 19:21:37 +0700

    nav bottom

--- resources/views/components/bottom-nav.blade.php
+++ resources/views/components/bottom-nav.blade.php
@@ -1,38 +1,38 @@
 <nav id="bottom-nav" class="navbar fixed-bottom navbar-light theme_bg border-white border-top p-2">
     <div class="d-flex w-100 justify-content-around white_fonts">
-        <a href="{{ url('/') }}" class="bottom-nav-item" data-toggle="tooltip" data-placement="bottom" title="Beranda">
+        <a href="{{ url('/') }}" style="{{ request()->is('/') ? 'border-bottom: solid 2px #2797db' : '' }}" class="bottom-nav-item" data-toggle="tooltip" data-placement="bottom" title="Beranda">
             <div class="text-center text-color">
-                <i class="fa fa-home"></i>
+                <i class="fa fa-home" style="{{ request()->is('/') ? 'color : #2797db' : '' }}"></i>
                 <div class="text-color text-nav-bottom">Home</div>
             </div>
         </a>
-        <a href="{{ url('tentang-kami') }}">
+        <a href="{{ url('tentang-kami') }}" style="{{ request()->is('tentang-kami') ? 'border-bottom: solid 2px #2797db' : '' }}">
             <div class="text-center text-color">
-                <i class="fa fa-building"></i>
+                <i class="fa fa-building" style="{{ request()->is('tentang-kami') ? 'color : #2797db' : '' }}"></i>
                 <div class="text-color text-nav-bottom">Tentang</div>
             </div>
         </a>
-        <a href="{{ url('layanan-kami') }}">
+        <a href="{{ url('layanan-kami') }}" style="{{ request()->is('layanan-kami') ? 'border-bottom: solid 2px #2797db' : '' }}">
             <div class="text-center text-color">
-                <i class="fa fa-paper-plane"></i>
+                <i class="fa fa-paper-plane" style="{{ request()->is('layanan-kami') ? 'color : #2797db' : '' }}"></i>
                 <div class="text-color text-nav-bottom">Layanan</div>
             </div>
         </a>
-        <a href="{{ url('portfolio') }}">
+        <a href="{{ url('portfolio') }}" style="{{ request()->is('portfolio') ? 'border-bottom: solid 2px #2797db' : '' }}">
             <div class="text-center text-color">
-                <i class="fa fa-briefcase"></i>
+                <i class="fa fa-briefcase" style="{{ request()->is('portfolio') ? 'color : #2797db' : '' }}"></i>
                 <div class="text-color text-nav-bottom">Portfolio</div>
             </div>
         </a>
-        <a href="{{ url('harga') }}">
+        <a href="{{ url('harga') }}" style="{{ request()->is('harga') ? 'border-bottom: solid 2px #2797db' : '' }}">
             <div class="text-center text-color">
-                <i class="fas fa-dollar"></i>
+                <i class="fas fa-dollar" style="{{ request()->is('harga') ? 'color : #2797db' : '' }}"></i>
                 <div class="text-color text-nav-bottom">Harga</div>
             </div>
         </a>
-        <a href="{{ url('kontak') }}">
+        <a href="{{ url('kontak') }}" style="{{ request()->is('kontak') ? 'border-bottom: solid 2px #2797db' : '' }}">
             <div class="text-center text-color">
-                <i class="fa fa-address-book"></i>
+                <i class="fa fa-address-book" style="{{ request()->is('kontak') ? 'color : #2797db' : '' }}"></i>
                 <div class="text-color text-nav-bottom">Kontak</div>
             </div>
         </a>
